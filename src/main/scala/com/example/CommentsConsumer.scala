309e3ba03aaff5a0a09fc9f1985bcc9e11e2c003
dusannistoran <dusan.nistoran@gmail.com>
2022-09-12 10:42:34 +0200

    commit 1

--- src/main/scala/com/example/CommentsConsumer.scala
+++ src/main/scala/com/example/CommentsConsumer.scala
@@ -3,21 +3,19 @@ package com.example
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 import org.slf4j.LoggerFactory
 
 class CommentsConsumer(topicNifi: String, broker: String) {
 
   val configSpark: Config = ConfigFactory.load().getConfig("application.spark")
   val sparkCores: String = configSpark.getString("master")
-  //val HDFS_HOME: String = configSpark.getString("hdfsHome")
   val checkpoint: String = configSpark.getString("checkpointLocation")
 
   lazy val spark = SparkSession
     .builder()
     .config("spark.speculation", "false")
     .config("checkpointLocation", s"$checkpoint")
-    //.config("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true")
     .master(s"$sparkCores")
     .appName("consume comments stream to console")
     .getOrCreate()
@@ -41,13 +39,14 @@ class CommentsConsumer(topicNifi: String, broker: String) {
       StructField("comment_author_channel_name", StringType) ::
       StructField("comment_author_channel_id", StringType) ::
       StructField("comment_like_count", IntegerType) ::
-      //StructField("published_at", StringType) ::
       StructField("published_at_timestamp", TimestampType) ::
       StructField("total_reply_count", IntegerType) :: Nil
   )
 
   val valueDf = df.selectExpr("CAST(value AS STRING)")
+
   import org.apache.spark.sql.functions._
+
   val dfWithColumns = valueDf
     .withColumn("value", from_json(col("value"), commentsSchema))
 
@@ -58,7 +57,6 @@ class CommentsConsumer(topicNifi: String, broker: String) {
     .withColumn("comment_author_channel_name", dfWithColumns.col("value.comment_author_channel_name"))
     .withColumn("comment_author_channel_id", dfWithColumns.col("value.comment_author_channel_id"))
     .withColumn("comment_like_count", dfWithColumns.col("value.comment_like_count"))
-    //.withColumn("published_at", dfWithColumns.col("value.published_at"))
     .withColumn("published_at", dfWithColumns.col("value.published_at_timestamp"))
     .withColumn("total_reply_count", dfWithColumns.col("value.total_reply_count"))
     .drop("value")
@@ -82,7 +80,6 @@ class CommentsConsumer(topicNifi: String, broker: String) {
     data.printSchema()
 
     import org.apache.spark.sql.functions.udf
-
 
     val countWordsFunction: UserDefinedFunction = udf((text: String) => {
       val cleanText = text.replaceAll("""[\p{Punct}]""", "")
